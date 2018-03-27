397e2803e9e88f494eb8003844885bba1d54a1f1
hpissei <hpultimatemedia@gmail.com>
2018-03-27 17:19:14 +0530

    march 27-03-2018 2

--- app/assets/javascripts/general/general.js
+++ app/assets/javascripts/general/general.js
@@ -6,3 +6,8 @@ document.getElementById("notification").addEventListener('click',function()
     document.getElementById("notification").style="display:none";
 });
 }
+
+function redirect_to(path)
+{
+    window.location=path;
+}
